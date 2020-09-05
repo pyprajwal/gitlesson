59c8179352b993bd4ece13fa3d885f24d08dd250
pyprajwal <prajwal.pokhrel63@gmail.com>
2020-09-05 21:26:28 +0545

    soso on featureZ

--- account.py
+++ account.py
@@ -2,7 +2,7 @@ def shoutname(name):
     print("hey " +name )
     Old = input("How old are you " +name+ "? ")
     print("you are " +Old+ " years old.")
-    print("myaan this is cool :D ")
+    print("myaan this is so so cool :D ")
 
 Name = input("Enter your Name : ")
 
