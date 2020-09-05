5073940000575cafc06588c029fc808f2a023cbd
pyprajwal <prajwal.pokhrel63@gmail.com>
2020-09-05 21:22:05 +0545

    featureZ commit

--- account.py
+++ account.py
@@ -2,7 +2,7 @@ def shoutname(name):
     print("hey " +name )
     Old = input("How old are you " +name+ "? ")
     print("you are " +Old+ " years old.")
-    print("myaan this is boring :( ")
+    print("myaan this is cool :D ")
 
 Name = input("Enter your Name : ")
 
