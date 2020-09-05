1a99e5232ebdcefd674b151a4265504e6b0fd3e2
pyprajwal <prajwal.pokhrel63@gmail.com>
2020-09-05 17:57:52 +0545

    "initial local commit"

---
+++ account.py
@@ -0,0 +1,9 @@
+def shoutname(name):
+    print("hello " +name )
+    Old = input("How old are you " +name+ "? ")
+    print("you are " +Old+ " years old.")
+    print("thank you")
+
+Name = input("Enter your Name : ")
+
+shoutname(Name)
